bd0a6c11b7a409f46346643f1d77b063f98a9abb
Zevilaardya03 <162274544+FarhanHusen@users.noreply.github.com>
2026-01-12 13:13:52 +0700

    app.py fix

--- app.py
+++ app.py
@@ -228,4 +228,5 @@ def delete_history(id):
     # Setelah hapus, kembali ke halaman riwayat
     return redirect(url_for("history"))
 
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    app.run(debug=True)
